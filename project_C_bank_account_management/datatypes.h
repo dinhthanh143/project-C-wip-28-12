5596f94ff4909b5e3c5bba494ddbcde448362d47
dinhthanh143 <neonlimea@gmail.com>
2024-12-28 04:12:12 +0700

    Add files via upload

---
+++ project_C_bank_account_management/datatypes.h
@@ -0,0 +1,14 @@
+#include<stdbool.h>
+struct Date{
+	int day,month,year;
+};
+struct User{
+	char userId[15];//10
+	char name[25];//20
+	char phone[15];//10
+	char email[25];//20
+	char password[15];//10
+	char status[15];//10
+};
+struct User users[100];
+struct Transaction;
